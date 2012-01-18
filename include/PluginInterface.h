50ca4b99f70570b1be74857e8d77a7a9da6f45eb
Andreas Heider <andreas@meetr.de>
2012-01-19 00:24:26 +0100

    plugininterface header

---
+++ include/PluginInterface.h
@@ -0,0 +1,44 @@
+#ifndef __PLUGIN_INTERFACE_H__
+#define __PLUGIN_INTERFACE_H__
+
+#include <QObject>
+#include <QStringList>
+
+class ParserInterface : public QObject
+{
+	Q_OBJECT
+
+public:
+	virtual ~ParserInterface() { };
+
+	virtual void parse(const QString &localPath, const QString &filename) = 0;
+};
+
+class PluginInterface
+{
+public:
+	struct MenuItem {
+		QString label;
+		int key;
+		bool checked;
+		MenuItem(QString const& l = "", int k = 0, bool c = false): label(l), key(k), checked(c) {}
+	};
+	struct MenuGroup {
+		QString label;
+		QList<MenuItem> items;
+	};
+
+	virtual ~PluginInterface() {};
+
+	virtual QStringList mimeTypes() = 0;
+	
+	virtual QStringList fileExtensions() { return QStringList(); };
+
+	virtual QWidget *reader(void* plugin_state, QWidget *parent = 0) = 0;
+
+	virtual ParserInterface *parser() = 0;
+};
+
+Q_DECLARE_INTERFACE(PluginInterface, "com.shortcovers.nickel.PluginInterface")
+
+#endif
