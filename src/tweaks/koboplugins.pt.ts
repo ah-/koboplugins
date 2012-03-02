61754c23c1395c43e43565c67d32fa5b783485ae
Andreas Heider <andreas@meetr.de>
2012-03-02 12:36:10 +0100

    Portuguese translation (thanks kyriu!)

---
+++ src/tweaks/koboplugins.pt.ts
@@ -0,0 +1,418 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT" sourcelanguage="en">
+<context>
+    <name>TweakView</name>
+    <message>
+        <location filename="tweakview.ui" line="14"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="39"/>
+        <source>TWEAKS</source>
+        <translation>TWEAKS</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="61"/>
+        <source>LEDS</source>
+        <translation>LEDS</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="89"/>
+        <source>Manually control the power LEDs.</source>
+        <translation>Controlo manual dos LEDs da bateria</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="127"/>
+        <source>Red</source>
+        <translation>Vermelho</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="154"/>
+        <location filename="tweakview.ui" line="251"/>
+        <location filename="tweakview.ui" line="300"/>
+        <source>On</source>
+        <translation>On</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="176"/>
+        <location filename="tweakview.ui" line="267"/>
+        <location filename="tweakview.ui" line="316"/>
+        <source>Off</source>
+        <translation>Off</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="203"/>
+        <source>Blue</source>
+        <translation>Azul</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="224"/>
+        <source>Green</source>
+        <translation>Verde</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="343"/>
+        <source>Home Menu</source>
+        <translation>Início</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="371"/>
+        <source>Add shortcuts to the home menu.</source>
+        <translation>Adicionar atalho.</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="436"/>
+        <source>Add browser shortcut</source>
+        <translation>Adicionar navegador</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="448"/>
+        <source>Miscellaneous Settings</source>
+        <translation>Ajustes diversos</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="476"/>
+        <source>Random stuff.</source>
+        <translation>Extras avulso</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="538"/>
+        <source>Enable wireless timeout</source>
+        <translation>Temporizador de WiFi</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="562"/>
+        <source>Hide recommendations</source>
+        <translation>Esconder as recomendações</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="578"/>
+        <source>Sync now</source>
+        <translation type="unfinished">Sincronizar agora</translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="602"/>
+        <source>font: 22px Avenir; border: 2px solid black; border-radius: 5px;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tweakview.ui" line="605"/>
+        <source>Uninstall Plugin</source>
+        <translation>Remover plugin</translation>
+    </message>
+</context>
+<context>
+    <name>TweaksPlugin</name>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="192"/>
+        <source>Library</source>
+        <translation type="unfinished">Biblioteca</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="204"/>
+        <source>Dictionary</source>
+        <translation type="unfinished">Dicionário</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="195"/>
+        <location filename="TweaksPlugin.cpp" line="221"/>
+        <location filename="TweaksPlugin.cpp" line="261"/>
+        <source>Shortlist</source>
+        <translation type="unfinished">Favoritos</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="198"/>
+        <location filename="TweaksPlugin.cpp" line="269"/>
+        <source>Bookshelves</source>
+        <translation type="unfinished">Estantes</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="201"/>
+        <location filename="TweaksPlugin.cpp" line="264"/>
+        <source>Library Search</source>
+        <translation type="unfinished">Procura</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="207"/>
+        <source>Reading Life</source>
+        <translation type="unfinished">Reading Life</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="210"/>
+        <source>Store</source>
+        <translation type="unfinished">Loja</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="213"/>
+        <source>Sync</source>
+        <translation type="unfinished">Sincronizar</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="216"/>
+        <source>Help</source>
+        <translation type="unfinished">Ajuda</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="218"/>
+        <source>Settings</source>
+        <translation type="unfinished">Parametros</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="225"/>
+        <source>Tweaks</source>
+        <translation type="unfinished">Ajustes</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="228"/>
+        <source>Browser</source>
+        <translation type="unfinished">Navegador</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="231"/>
+        <source>Airplane Mode</source>
+        <translation type="unfinished">Modo Avião</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="234"/>
+        <source>Toggle WiFi</source>
+        <translation type="unfinished">WiFi On/Off</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="237"/>
+        <source>Power Off</source>
+        <oldsource>Power off</oldsource>
+        <translation type="unfinished">Desligar</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="258"/>
+        <source>Books</source>
+        <translation type="unfinished">Livros</translation>
+    </message>
+    <message>
+        <location filename="TweaksPlugin.cpp" line="268"/>
+        <source>Create Shelf</source>
+        <translation type="unfinished">Criar estantes</translation>
+    </message>
+</context>
+<context>
+    <name>TweaksSettingsHomeMenuView</name>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="14"/>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="29"/>
+        <source>Home Menu</source>
+        <translation type="unfinished">Menu Inicial</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="57"/>
+        <source>Change the content of the START menu.</source>
+        <translation type="unfinished">Modificar menu de AJUDA</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="173"/>
+        <source>Airplane Mode</source>
+        <translation type="unfinished">Modo Avião</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="341"/>
+        <source>Browser</source>
+        <translation type="unfinished">Navegador</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="149"/>
+        <source>Dictionary</source>
+        <translation type="unfinished">Dicionário</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="125"/>
+        <source>Library Search</source>
+        <translation type="unfinished">Pesquisa</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="317"/>
+        <source>Help</source>
+        <translation type="unfinished">Ajuda</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="245"/>
+        <source>Reading Life</source>
+        <translation type="unfinished">Reading Life</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="269"/>
+        <source>Store</source>
+        <translation type="unfinished">Loja</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="101"/>
+        <source>Shortlist</source>
+        <translation type="unfinished">Favoritos</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="293"/>
+        <source>Sync</source>
+        <translation type="unfinished">Sincronizar</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="221"/>
+        <source>Power Off</source>
+        <translation type="unfinished">Desligar</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsHomeMenuView.ui" line="197"/>
+        <source>Toggle WiFi</source>
+        <translation type="unfinished">WiFi On/Off</translation>
+    </message>
+</context>
+<context>
+    <name>TweaksSettingsLEDView</name>
+    <message>
+        <source>Form</source>
+        <translation type="obsolete">Form</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsLEDView.ui" line="14"/>
+        <location filename="TweaksSettingsLEDView.ui" line="29"/>
+        <source>LED CONTROL</source>
+        <translation type="unfinished">LED CONTROL</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsLEDView.ui" line="57"/>
+        <source>Manually control the power LEDs.</source>
+        <translation type="unfinished">Controlo manual dos LEDs da bateria</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsLEDView.ui" line="101"/>
+        <source>Green</source>
+        <translation type="unfinished">Verde</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsLEDView.ui" line="125"/>
+        <source>Red</source>
+        <translation type="unfinished">Vermelho</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsLEDView.ui" line="149"/>
+        <source>Blue</source>
+        <translation type="unfinished">Azul</translation>
+    </message>
+</context>
+<context>
+    <name>TweaksSettingsMiscView</name>
+    <message>
+        <source>Form</source>
+        <translation type="obsolete">Form</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="14"/>
+        <source>MISCELLANEOUS</source>
+        <translation type="unfinished">Diversos</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="29"/>
+        <source>Miscellaneous</source>
+        <translation type="unfinished">Diversos</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="57"/>
+        <source>Hide recommendations on the Homescreen.</source>
+        <translation type="unfinished">Esconder recomendações na
+	página inícial</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="101"/>
+        <source>Hide recommendations</source>
+        <translation type="unfinished">Esconder recomendações</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="125"/>
+        <source>Hide page numbers</source>
+        <translation type="unfinished">Esconder número de página</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="143"/>
+        <source>Experimental</source>
+        <translation type="unfinished">Experimental</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="174"/>
+        <source>Enable Shelves. Be careful, this feature is not finished and might corrupt your database.</source>
+        <translation type="unfinished">Activar estantes. Esta função é
+	experimental e pode corromper a base de dados.</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="218"/>
+        <source>Enable Shelves</source>
+        <translation type="unfinished">Activar estantes</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="236"/>
+        <source>Uninstall</source>
+        <translation type="unfinished">Remover</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="267"/>
+        <source>Uninstall the Tweaks plugin and reboot the device.</source>
+        <translation type="unfinished">Desinstalar plugin e reiniciar..</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="289"/>
+        <source>font: 22px Avenir; border: 2px solid black; border-radius: 5px;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsMiscView.ui" line="292"/>
+        <source>Uninstall Plugin</source>
+        <translation type="unfinished">Remover</translation>
+    </message>
+</context>
+<context>
+    <name>TweaksSettingsPageHeader</name>
+    <message>
+        <location filename="TweaksSettingsPageHeader.ui" line="32"/>
+        <source>Form</source>
+        <translation type="unfinished">Form</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsPageHeader.ui" line="76"/>
+        <source>TWEAKS</source>
+        <translation type="unfinished">TWEAKS</translation>
+    </message>
+</context>
+<context>
+    <name>TweaksSettingsPageView</name>
+    <message>
+        <location filename="TweaksSettingsPageView.ui" line="14"/>
+        <source>TWEAKS</source>
+        <translation type="unfinished">TWEAKS</translation>
+    </message>
+</context>
+<context>
+    <name>TweaksSettingsView</name>
+    <message>
+        <source>Form</source>
+        <translation type="obsolete">Form</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsView.ui" line="14"/>
+        <source>TWEAKS</source>
+        <translation type="unfinished">TWEAKS</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsView.ui" line="42"/>
+        <source>Home Menu</source>
+        <translation type="unfinished">Menu Inicial</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsView.ui" line="68"/>
+        <source>Miscellaneous</source>
+        <translation type="unfinished">Diversos</translation>
+    </message>
+    <message>
+        <location filename="TweaksSettingsView.ui" line="94"/>
+        <source>LEDs</source>
+        <translation type="unfinished">LEDs</translation>
+    </message>
+</context>
+</TS>
